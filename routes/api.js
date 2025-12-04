5f58eef3f138d11cbb0edc335df601a25b9bdb57
Lmaleque <lmale001@campus.goldsmiths.ac.uk>
2025-12-04 21:33:23 +0000

    lab9 with weather + api books route

---
+++ routes/api.js
@@ -0,0 +1,27 @@
+// Import express module and create a router object
+const express = require("express")
+const router = express.Router()
+
+
+// Import express module
+router.get('/books', function (req, res, next) {
+
+    // Query database to get all the books
+    let sqlquery = "SELECT * FROM books"
+
+    // Execute the sql query
+    db.query(sqlquery, (err, result) => {
+        // Return results as a JSON object
+        if (err) {
+            res.json(err)
+            next(err)
+        }
+        else {
+            res.json(result)
+        }
+    })
+})
+
+
+// Export the router object so index.js can access it
+module.exports = router
